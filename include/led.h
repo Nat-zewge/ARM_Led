cea507866a672395ccfc5bc6430a0fa8267a3905
Nat-zewge <nzewge35@gmail.com>
2016-12-28 23:42:40 +0900

    initial commit

---
+++ include/led.h
@@ -0,0 +1,29 @@
+/*
+ * led.h
+ *
+ *  Created on: Dec 28, 2016
+ *      Author: natnael
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+
+void led_init(void); //initialize
+
+
+void ledBlinkN(int num); // blink N-times
+
+void blinkOnce(void);
+
+void led_on(void);
+
+void led_off(void);
+
+
+
+
+
+
+
+#endif /* LED_H_ */
---
+++ src/led.c
@@ -0,0 +1,8 @@
+/*
+ * led.c
+ *
+ *  Created on: Dec 28, 2016
+ *      Author: natnael
+ */
+
+
